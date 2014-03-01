3042f655c2e471ec034e5229e84ad0e608f3189a
Marcos Lin <linm.linmair@ebacimail.com>
2014-03-01 19:18:50 +0100

    Added intro2

--- intro/js/controller/intro_ctrl.js
+++ intro/js/controller/intro_ctrl.js
@@ -73,3 +73,62 @@ app.controller('Intro1Controller', ['$scope', '$log', function ($scope, $log) {
     updateChart(init_data);
 
 }]);
+
+/**
+ * INTRO2
+ */
+app.controller('Intro2Controller', ['$scope', '$log', function ($scope, $log) {
+    var diameter = 800,
+        format = d3.format(",d"),
+        color = d3.scale.category20c();
+    
+    var bubble = d3.layout.pack()
+        .sort(null)
+        //.size([diameter, diameter])
+        .size([diameter, diameter])
+        .padding(1.5);
+    
+    var svg = d3.select("body").append("svg")
+        .attr("width", diameter)
+        .attr("height", diameter)
+        .attr("class", "bubble");
+    
+    d3.json("data/flare.json", function(error, root) {
+      var node = svg.selectAll(".node")
+          .data(bubble.nodes(classes(root))
+          .filter(function(d) { return !d.children; }))
+        .enter().append("g")
+          .attr("class", "node")
+          .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+    
+        $log.log("classes: ", classes(root));
+        
+      node.append("title")
+          .text(function(d) { return d.className + ": " + format(d.value); });
+    
+      node.append("circle")
+          .attr("r", function(d) { return d.r; })
+          .style("fill", function(d) { return color(d.packageName); });
+    
+      node.append("text")
+          .attr("dy", ".3em")
+          .style("text-anchor", "middle")
+          .text(function(d) { return d.className.substring(0, d.r / 3); });
+    });
+    
+    // Returns a flattened hierarchy containing all leaf nodes under the root.
+    function classes(root) {
+      var classes = [];
+    
+      function recurse(name, node) {
+        if (node.children) node.children.forEach(function(child) { recurse(node.name, child); });
+        else classes.push({packageName: name, className: node.name, value: node.size});
+      }
+    
+      recurse(null, root);
+      return {children: classes};
+    }
+    
+    d3.select(self.frameElement).style("height", diameter + "px");
+    
+}]);
