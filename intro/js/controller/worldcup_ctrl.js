8108777347eb00112032c66a9628fa6da5213f12
Marcos Lin <linm.linmair@ebacimail.com>
2014-03-03 14:22:51 +0100

    Working version of sample Axis

--- intro/js/controller/worldcup_ctrl.js
+++ intro/js/controller/worldcup_ctrl.js
@@ -2,84 +2,20 @@ angular.module("d3jsapp")
 .controller('WorldcupController', ['$scope', 'Team', '$timeout', '$log', function ($scope, Team, $timeout, $log) {
     
     // Define size of the graph
-    var svg = d3.select(".team_chart"),
+    var circle_canvas_width = 950,  // From style.css
+        circle_canvas_height = 550, // From style.css
         color = d3.scale.category10(),
-        width = 700,
-        height = 500,
-        x_offset = 100,
-        y_offset = 50;
+        team_data;
     
     Team.then(function (data) {
         $log.debug("[WorldcupController] team data loaded");
-        // $scope.teams = data;
-        drawTeams(data);
-        drawCircle(data);
-        $log.debug("[WorldcupController] draw completed");
-    })
-    
-    function randomize () {
-        return function (d) {
-            var targetX = width / 2;
-            var targetY = height / 2;
-            d.x += (targetX - d.x) * 0.1;
-            d.y += (targetY - d.y) * 0.1;
-        }
-    }
-    
-    function totalLayout(force) {
-        // Generate the effect that centralize bubbles in the center of the canvas
-        force
-            .gravity(2)
-            .friction(0.4)
-            .on("tick", function(e) {
-                svg.selectAll("circle")
-                .each(randomize())
-                .attr("cx", function(d) { return d.x + x_offset; })
-                .attr("cy", function(d) { return d.y + y_offset; });
-            })
-            .start();
+        // Store the data
+        team_data = data;
         
-        for (var i=0; i<100; i++) force.tick();
-        
-        $timeout(function () {
-            force.stop();
-            force
-                .gravity(0.6) //0.6
-                .friction(0.2) // 0.2
-            .on("tick", function(e){
-                svg.selectAll("circle")
-                .attr("cx", function(d) { return d.x + x_offset; })
-                .attr("cy", function(d) { return d.y + y_offset; });
-            }).start();
-        }, 700);
-        
-    }
-    
-    var team_orig_position = {};
-    function teamMouseOver(d, i) {
-        var idclass = d.circle_idclass,
-            circle = d3.select("#" + idclass);
-        circle.classed("highlight", true);
-        
-    }
-    function teamMouseOut(d, i) {
-        var idclass = d.circle_idclass,
-            circle = d3.select("#" + idclass);
-        circle.classed("highlight", false);
-    }
-    function drawTeams(data) {
-        /*
-        <div class="team">
-            <span class="flagsp flagsp_civ"></span>
-            <span class="team-code">CIV</span>
-        </div>
-        */
-        
-        var flags = [];
-        
-        // Create a copy
+        // Create data for displaying team flag
+        var team_flag_data = [];
         angular.forEach(data, function (d) {
-            flags.push({
+            team_flag_data.push({
                 team_name: d.team_name,
                 team_code: d.fifa_code.toUpperCase(),
                 team_idclass: d.team_idclass,
@@ -87,6 +23,29 @@ angular.module("d3jsapp")
                 flag_class: d.flag_class
             }); 
         });
+        
+        drawTeams(team_flag_data);
+        drawAxis();
+        drawCircle(data);
+        $log.debug("[WorldcupController] draw completed");
+    })
+    
+    
+    /** 
+     * TEAM SECTION
+     */
+    var team_orig_position = {};
+    function teamMouseOver(d, i) {
+        var idclass = d.circle_idclass,
+            circle = d3.select("#" + idclass);
+        circle.classed("highlight", true);
+    }
+    function teamMouseOut(d, i) {
+        var idclass = d.circle_idclass,
+            circle = d3.select("#" + idclass);
+        circle.classed("highlight", false);
+    }
+    function drawTeams(data) {
         var grid = d3.layout.grid()
             .bands()
             .nodeSize([4, 8])
@@ -94,11 +53,10 @@ angular.module("d3jsapp")
             .cols(16)
             .padding([55, 17]);
         
-        var flagNodes = grid(flags),
-            team_flags = d3.select("#team-flags"),
+        var team_flags = d3.select("#flag-canvas"),
             team_flag = team_flags
                 .selectAll("div")
-                .data(flagNodes)
+                .data(grid(data))
                 .enter()
                 .append("div")
                 .attr("class", "team")
@@ -130,6 +88,46 @@ angular.module("d3jsapp")
         
     }
     
+    /** 
+     * CIRCLE SECTION
+     */
+    function randomize () {
+        return function (d) {
+            var targetX = circle_canvas_width / 2;
+            var targetY = circle_canvas_height / 2;
+            d.x += (targetX - d.x) * 0.1;
+            d.y += (targetY - d.y) * 0.1;
+        }
+    }
+    
+    function totalLayout(team_chart, force) {
+        // Generate the effect that centralize bubbles in the center of the canvas
+        force
+            .gravity(2)
+            .friction(0.4)
+            .on("tick", function(e) {
+                team_chart.selectAll("circle")
+                .each(randomize())
+                .attr("cx", function(d) { return d.x; })
+                .attr("cy", function(d) { return d.y; });
+            })
+            .start();
+        
+        for (var i=0; i<100; i++) force.tick();
+        
+        $timeout(function () {
+            force.stop();
+            force
+                .gravity(0.6) //0.6
+                .friction(0.2) // 0.2
+            .on("tick", function(e){
+                team_chart.selectAll("circle")
+                .attr("cx", function(d) { return d.x; })
+                .attr("cy", function(d) { return d.y; });
+            }).start();
+        }, 700);
+        
+    }
     
     // OnMouseHover bubble effect
     function circleMouseOver (d, i) {
@@ -167,18 +165,18 @@ angular.module("d3jsapp")
                 return rankingX(d.fifa_rank) * -120;
             })
             .nodes(data)
-            .size([width, height]);
+            .size([circle_canvas_width, circle_canvas_height]);
         force.start();
-        
-        
-        
-        var circles = svg.selectAll("circle");
+
+        var team_chart = d3.select("#team-chart").append("g"),
+            circles = team_chart.selectAll("circle");
+
         circles
             .data(data)
             .enter()
             .append("circle")
-            .attr("cx", function(d) { return d.x + x_offset; })
-            .attr("cy", function(d) { return d.y + y_offset; })
+            .attr("cx", function(d) { return d.x; })
+            .attr("cy", function(d) { return d.y; })
             .attr("r", function (d) {
                 // Ranking should be the area of the circle
                 // return Math.sqrt(rankingX(d.fifa_rank)/Math.PI)
@@ -197,11 +195,62 @@ angular.module("d3jsapp")
             })
         ;
         
-        totalLayout(force);
+        totalLayout(team_chart, force);
     }
     
+    /** 
+     * AXIS Section
+     */
+    var priv_showAxis = true;
+    $scope.showAxis = function () {
+        var axis = d3.selectAll(".axis");
+        priv_showAxis = !priv_showAxis;
+        axis.classed("show", priv_showAxis);
+    };
     
     
+    function drawAxis() {
+        $log.log("Axis called.");
+        var margin = { top: 20, left: 50 },
+            width = circle_canvas_width - margin.left,
+            height = circle_canvas_height - margin.top,
+            x = d3.scale.linear()
+                .domain([-width / 2, width / 2])
+                .range([0, width]);
+        
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom")
+            .tickSize(-height);
+        
+        var y = d3.scale.linear()
+            .domain([-height / 2, height / 2])
+            .range([height, 0]);
+        
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .ticks(5)
+            .tickSize(-width);
+        
+        var axis_g = d3.select("#team-chart")
+            .append("g")
+            .attr("transform", "translate(" + margin.left + ",-" + margin.top + ")")
+
+        axis_g
+            .append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .attr("id", "xaxis")    
+            .attr("class", "axis")
+            .call(xAxis);
+        
+        axis_g
+            .append("g")
+            .attr("id", "yaxis")
+            .attr("class", "y axis")
+            .call(yAxis);
+    
+    }
     
     
 }]);
